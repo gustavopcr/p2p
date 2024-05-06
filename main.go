328848d5affde90dac72155e509e890df585dec3
gustavopcr <gustavopc2501@gmail.com>
2024-05-06 18:49:20 -0300

    still figuring it out

--- main.go
+++ main.go
@@ -4,20 +4,46 @@ import (
 	"fmt"
 	"net"
 	"os"
+	"os/signal"
+    "syscall"
 )
 
-func main() {
-	data, err := os.ReadFile("teste3.7z")
-	if err != nil {
-		fmt.Printf("Erro ao ler arquivo: %s\n", err)
-	}
+func handleRequest(conn net.Conn) {
+    defer conn.Close()
+	// Handle incoming request
+    // For simplicity, just send a byte slice back to the client
+    //data := []byte("Hello, peer!")
+    //conn.Write(data)
+}
 
-	conn, err := net.Dial("tcp", "localhost:3000")
+
+func startServer() {
+	l, err := net.Listen("tcp", ":6060")
 	if err != nil {
-		fmt.Printf("Error dialing TCP: %s\n", err)
+		fmt.Println("Error listening:", err)
 		return
 	}
-	defer conn.Close()
+	defer l.Close()
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println("error")
+		}
+		fmt.Println("go handleRequest(conn)")
+		go handleRequest(conn)
+	}
+}
 
-	sendData(len(data)/2, &data, &conn)
+func main() {
+	go startServer()
+
+	// Start CLI
+	fmt.Println("CLI is running...")
+
+	// Wait for interrupt signal (Ctrl+C) to exit the program
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+
+	fmt.Println("Exiting...")
 }
