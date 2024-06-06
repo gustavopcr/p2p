5bd45fa04b50e2632d8742de52df59d6b20f049a
gustavopcr <gustavopc2501@gmail.com>
2024-06-06 17:10:50 -0300

    reading peers from nexus

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -2,15 +2,26 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"github.com/gustavopcr/p2p/internal/server"
+	"github.com/gustavopcr/p2p/types"
+	"time"
+	"encoding/json"
 )
 
 func main() {
-	//server.
-	go server.StartServer()
+	var peers = make([]types.Peer, 0)
+	go server.StartServer()	
 	fmt.Println("p2p running...")
 	
 	for {
+		r, err := http.Get("http://localhost:8080/peers")
+		if err != nil{
+			panic(err)
+		}
+		err = json.NewDecoder(r.Body).Decode(&peers)
+		fmt.Println(peers)
+		time.Sleep(30 * time.Second)
 	}
 	
 }
